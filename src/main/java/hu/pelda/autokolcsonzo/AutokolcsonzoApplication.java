d706e300d74a08b01eb5a34f0d944857371fe1b1
Szagmaiszter Ábel <h164296@stud.u-szeged.hu>
2026-06-15 19:14:11 +0200

    A projekt létrehozása és a mappák elredezése

--- src/main/java/hu/pelda/autokolcsonzo/AutokolcsonzoApplication.java
+++ src/main/java/hu/pelda/autokolcsonzo/AutokolcsonzoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AutokolcsonzoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AutokolcsonzoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AutokolcsonzoApplication.class, args);
+    }
 
 }
